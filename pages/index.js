0c01d8e0b7b0388fe8c8e2890955237cdeba2396
call5h5ong <call5h5ong@gmail.com>
2022-01-07 11:57:57 +0900

    #1.2 Static Pre Rendering

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 export default function Hello() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div>
-      <h1>Hello!</h1>
+      <h1>Hello! {counter}</h1>
+      <button onClick={() => setCounter((p) => p + 1)}>+</button>
     </div>
   );
 }
