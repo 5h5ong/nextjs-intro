76a0551beaac72067ba21ef5cfd6cdf21b408420
call5h5ong <call5h5ong@gmail.com>
2022-01-08 10:12:32 +0900

    #2.0 Patterns

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
 import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
 
 export default function AboutAs() {
   return (
     <div>
+      <Seo title="About" />
       <h1>about</h1>
     </div>
   );
