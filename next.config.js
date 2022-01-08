4d3bc2f225f6c79354d18dabcba15094071b889d
call5h5ong <call5h5ong@gmail.com>
2022-01-08 12:41:54 +0900

    #2.2 Redirect and Rewrite

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/contact/:path",
+        destination: "/form/:path",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`,
+      },
+    ];
+  },
+};
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,53 @@
+import { useState, useEffect } from "react";
 import Seo from "../components/Seo";
 
 export default function Hello() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`/api/movies`);
+      const { results } = await response.json();
+      setMovies(results);
+    })();
+  }, []);
   return (
-    <div>
+    <div className="container">
       <Seo title="Home" />
-      <h1>Hello! </h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id} className="movie">
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+          justify-items: center;
+        }
+        .movie {
+          display: flex;
+          width: 50%;
+          flex-direction: column;
+          justify-items: center;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
