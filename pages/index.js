60bf3c203ed9c1e8c7115819cc36cc3398098262
call5h5ong <call5h5ong@gmail.com>
2022-01-09 15:28:41 +0900

    2.6 Movie Detail

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,45 @@
-import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Seo from "../components/Seo";
 
 export default function Hello({ results }) {
+  const router = useRouter();
+
+  const goToMoviesDetail = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
+  };
+
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div key={movie.id} className="movie">
+        <div
+          className="movie"
+          key={movie.id}
+          onClick={() => goToMoviesDetail(movie.id, movie.original_title)}
+        >
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <Link
+            href={{
+              pathname: `/movies/${movie.id}`,
+              query: {
+                title: movie.original_title,
+              },
+            }}
+            as={`/movies/${movie.id}`}
+          >
+            <a>
+              <h4>{movie.original_title}</h4>
+            </a>
+          </Link>
         </div>
       ))}
       <style jsx>{`
--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -2,6 +2,11 @@ import { useRouter } from "next/router";
 
 export default function Detail() {
   const router = useRouter();
-  console.log(router);
-  return "detail";
+
+  return (
+    <div>
+      <h4>{router.query.title ?? "Loading..."}</h4>
+      <h5>{router.query.id ?? "Loading..."}</h5>
+    </div>
+  );
 }
